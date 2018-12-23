96862dfdbcd69b54cc04879049bc3a115e3ef54d
Nimish Mishra <neelam.nimish@gmail.com>
2018-12-23 11:38:53 +0530

    Define constants to be used

--- constants.py
+++ constants.py
@@ -1,4 +1,7 @@
 import numpy as np
+
+# This array defines the vertices of the shape we want our image to have.
+# Every pixel outside this shape will be darkened.
 path_vertices = np.array([[0, 600],
             [0, 272],
             [240, 172],
